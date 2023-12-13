133cd991cca4afec993b7706b33a2867a041fa29
Theodore Kyenkyenhene <tkyenkyenhene21@gmail.com>
2023-12-13 02:59:34 -0500

    Final updates

--- users/schema.js
+++ users/schema.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
     password: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    likedSongs: [songSchema],
+    likedSongs: [],
   },
   { collection: "users" }
 );
